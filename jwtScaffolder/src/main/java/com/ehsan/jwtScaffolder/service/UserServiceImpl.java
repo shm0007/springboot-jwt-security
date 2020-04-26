06a04eb75e50e8e95a4f8b41248d6cde56cb6859
shm0007 <ehsanhrid@gmail.com>
2020-04-27 01:30:11 +0600

    added password encryption

--- jwtScaffolder/src/main/java/com/ehsan/jwtScaffolder/service/UserServiceImpl.java
+++ jwtScaffolder/src/main/java/com/ehsan/jwtScaffolder/service/UserServiceImpl.java
@@ -6,12 +6,17 @@ import com.ehsan.jwtScaffolder.model.ErrorResponse;
 import com.ehsan.jwtScaffolder.model.RegistrationRequest;
 import com.ehsan.jwtScaffolder.model.UsersResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements  UserService{
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public UsersResponse findUsers(){
 
         return UsersResponse.builder().users(userRepository.findAll()).build();
@@ -20,7 +25,7 @@ public class UserServiceImpl implements  UserService{
     public Object registerUser(RegistrationRequest req){
         User user = User.builder().email(req.getEmail())
                 .name(req.getName())
-                .password(req.getPassword())
+                .password(passwordEncoder.encode(req.getPassword()))
                 .role(req.getRole().getValue())
                 .build();
         if(userRepository.existsByEmail(user.getEmail()))
