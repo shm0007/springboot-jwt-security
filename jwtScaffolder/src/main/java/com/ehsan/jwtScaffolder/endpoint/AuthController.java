372a5dab9b7cec08c26dd00bfe22979c868cbeee
mhNirob <smh.nirob@gmail.com>
2020-04-27 19:16:34 +0600

    React first commit

--- jwtScaffolder/src/main/java/com/ehsan/jwtScaffolder/endpoint/AuthController.java
+++ jwtScaffolder/src/main/java/com/ehsan/jwtScaffolder/endpoint/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/auth")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthController {
     @Autowired
     UserService userService;
